c5a7e031a18eb017e389d91be351b7bbda8f245c
morodeer <morodeer@gmail.com>
2017-07-11 01:52:31 +0300

    Moved to installing by bundler by default

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 cd "$(dirname "$0")"
 export RAILS_ENV=production
 export GEM_HOME="$(pwd)/vendor"
-bundle="./vendor/gems/bundler-1.15.1/exe/bundle"
 
-if [ "$1" = "--with-install" ]; then
+if [ ! "$1" = "--without-install" ]; then
 
     #try gcc
     if [ -z $(which gcc) ]; then
@@ -18,24 +17,22 @@ if [ "$1" = "--with-install" ]; then
             error="MAKE_NOT_FOUND"
     fi
 
+    if [ -z $(which bundle) ]; then
+        echo "bundle is not found, please make sure it's installed and in PATH"
+        error="BUNDLE_NOT_FOUND"
+    fi
+
 
     if [ -n "$error" ]; then
             exit 1
     fi
 
-    echo "Installing bundler...\n"
-
-
-    bundler=$(gem install bundler --no-ri --no-rdoc | grep -o "bundler-[0-9\.]\+")
-    echo $bundler
-    ./vendor/gems/$bundler/exe/bundle install --path vendor/bundle --without development test
+    bundle install --path vendor/bundle --without development test
 fi
 
-
-
-export SECRET_KEY_BASE=$($bundle exec rake secret)
-$bundle exec rake assets:precompile
-$bundle exec rails server
+export SECRET_KEY_BASE=$(bundle exec rake secret)
+bundle exec rake assets:precompile
+bundle exec rails server
 
 
 
